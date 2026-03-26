8f8861ac5429d142c72897dbc79353f172f559ba
Daniel Aspinwall <dbaspinwall1@gmail.com>
2026-03-26 16:19:37 -0400

    Begin VainLib

---
+++ VainLib/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace VainLib;
+
+[Plugin(Name)]
+public class Plugin
+{
+    public const string Author = "Vainstains";
+    public const string Name = "VainLib";
+    public const string ID = $"com.{Author}.{Name}";
+
+    [Init]
+    private void Init()
+    {
+        new Harmony(ID)
+            .PatchAll(Assembly.GetExecutingAssembly());
+
+        Debug.Log($"Hello from {Name}!");
+    }
+}
+
+public enum CMScene
+{
+    FirstBoot = 0,
+    SongSelectMenu = 1,
+    SongEditMenu = 2,
+    Mapper = 3,
+    Options = 4
+}
+
+public static class CMSceneExtensions
+{
+    public static Scene GetUnityScene(this CMScene scene) => SceneManager.GetSceneByBuildIndex((int)scene);
+}
